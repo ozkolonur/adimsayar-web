7a359ba4461e7f62a398e8022a581324a0fd802e
root <root@ip-172-30-0-13.eu-west-1.compute.internal>
2017-01-24 18:13:52 +0000

    remove passwords

--- settings.py
+++ settings.py
@@ -17,7 +17,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'app_adimsayar',                    # Or path to database file if using sqlite3.
         'USER': 'app_adimsayar',                      # Not used with sqlite3.
-        'PASSWORD': 'OypQd2X74VpWyAxK',                  # Not used with sqlite3.
+        'PASSWORD': '****************',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
